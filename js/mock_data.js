f4d8160a043da14cf8e801a6801564ab3d2ef887
Win Sandy Myint <2322490+winsandymyint@users.noreply.github.com>
2021-06-17 23:46:17 +0630

    Create Mock.js

---
+++ js/mock_data.js
@@ -0,0 +1,12 @@
+let predefinedData = {
+    email: 'loki@gmail.com',
+    password: '12345678',
+}
+
+
+let predefinedData = {
+    item: 'loki@gmail.com',
+    isInStock: '12345678',
+    price: '12345678',
+    image: '12345678',
+}
